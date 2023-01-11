17610e14a00daa5058c077f21e65294db9a835cf
maverick <priyanshujha200@gmail.com>
2023-01-11 19:52:30 +0530

    initial commit

---
+++ Day3/Easy/answer1.js
@@ -0,0 +1,16 @@
+var firstName = 'Priyanshu';
+var lastName = 'Jha';
+var country = 'India';
+var city = 'DGR';
+var age = 20;
+var isMarried = false ;
+var year = 2023;
+
+console.log(typeof(firstName));
+console.log(typeof(lastName));
+console.log(typeof(country));
+console.log(typeof(city));
+console.log(typeof(age));
+console.log(typeof(isMarried));
+console.log(typeof(year));
+
